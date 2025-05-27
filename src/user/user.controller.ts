720ca745ec43ff22cb22c2121b7a49cbdce7407f
alima987 <alima1996b@gmail.com>
2025-05-27 12:40:18 +0500

    feat: implement get id and post req for users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Get, HttpCode } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, NotFoundException, Param, ParseUUIDPipe, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "./user.entity";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { instanceToPlain } from "class-transformer";
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('user')
+  @Get()
   @HttpCode(200)
   findAll(): User[] {
     return this.userService.findAll()
+  }
+  @Get(':id')
+  @HttpCode(200)
+  findById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): User {
+    const user = this.userService.findById(id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
+  }
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  async create(@Body() createUserDto: CreateUserDto) {
+    const newUser = this.userService.create(createUserDto)
+    return instanceToPlain(newUser)
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { User } from "./user.entity";
 import { users } from "./user.storage";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UserService {
   private users = users
   findAll(): User[] {
     return this.users
+  }
+  findById(id: string): User | undefined {
+    return this.users.find(u => u.id === id);
+  }
+  create(createUserDto: CreateUserDto): User {
+    const newUser = new User({
+      id: uuidv4(),
+      login: createUserDto.login,
+      password: createUserDto.password,
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    })
+    this.users.push(newUser);
+    return newUser
   }
 }
